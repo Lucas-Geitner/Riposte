b869b13ae111be55bab37dfedb3141c9310f7748
Lucas geitner <lucas.geitner@outlook.com>
2016-11-24 13:26:23 +0100

    Test settings

--- server/main.js
+++ server/main.js
@@ -2,4 +2,6 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  console.log(Meteor.settings.private.github, 'private');
+  console.log(Meteor.settings.public.analytics, 'public');
 });
