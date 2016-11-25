cc0875694d76650967ff51cf995d1db3a2afcd84
Lucas geitner <lucas.geitner@outlook.com>
2016-11-25 18:38:04 +0100

    update ugly

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,31 +1,50 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-
 import { Tweets } from '../api/tweets';
 import Tweet from './tweet.js';
 
-
 class App extends Component {
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = this.name.value.trim();
+    Tweets.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+    this.name.value = '';
+  }
+
   renderTweet() {
-    return this.props.tweets.map((tweet) => (
+    return this.props.tweets.map(tweet => (
       <Tweet key={tweet.id} tweet={tweet} />
-    ));
+      ));
   }
 
   render() {
     return (
       <div className="container">
-        <header>
-          <h1>Benoit Hamon</h1>
-          <ul>
-            {this.props.tweets.map((tweet) => (
-              <Tweet key={tweet._id} tweet={tweet} />
-            ))}
-          </ul>
-        </header>
+      <header>
+      <h1>Benoit Hamon</h1>
+      </header>
+      <div className="tweets-container">
+      <form className="tweet-order box" onSubmit={e => this.handleSubmit(e)} >
+      <input
+      type="text"
+      ref={input => this.name = input}
+      placeholder="Type to add new tasks"
+      />
+      <div className="commentaireBox">
+      <div className="Commentaire">
+      {this.props.tweets.map(tweet => (
+        <Tweet key={tweet._id} tweet={tweet} />
+        ))}
       </div>
-    );
+      </div>
+      </form>
+      </div>
+      </div>
+      );
   }
 }
 
@@ -33,8 +52,7 @@ App.propTypes = {
   tweets: PropTypes.array.isRequired,
 };
 
-export default createContainer(() => {
-  return {
-    tweets: Tweets.find({}).fetch(),
-  };
-}, App);
+export default createContainer(() => ({
+  tweets: Tweets.find({}, { sort: { createdAt: -1 } }).fetch(),
+
+}), App);
--- imports/ui/tweet.js
+++ imports/ui/tweet.js
@@ -1,12 +1,50 @@
 import React, { Component, PropTypes } from 'react';
+import { Tweets } from '../api/tweets';
 
+class Tweet extends Component {
+  toggleChecked() {
+    Tweets.update(this.props.tweet._id, {
+      $set: { checked: !this.props.tweet.checked },
+    });
+  }
+  deleteTask() {
+    Tweets.remove(this.props.tweet._id);
+  }
+  updateTweet(e) {
+    e.preventDefault();
+    const text = this.name.value.trim();
+    Tweets.update(this.props.tweet._id, {
+      $set: { text },
+    });
+  }
+  render() {
+    const { text, id } = this.props.tweet;
+    const tweetAprove = this.props.tweet.checked ? 'hide' : '';
+    return (
+      <div className ={tweetAprove + 'box'}>
+        <button className="delete" onClick={e => this.deleteTask()}>
+            &times;
+        </button>
 
-const Tweet = (props) => (
-  <li key={props.id}>
-    {props.tweet.text}
-  </li>
-
-);
+        <input
+          type="checkbox"
+          className="count"
+          readOnly
+          checked={this.props.tweet.checked}
+          onClick={e => this.toggleChecked()}
+        />
+        <form onSubmit={e => this.updateTweet(e)}>
+          <input
+            type="text"
+            className="onUpdateTweet"
+            defaultValue={text}
+            ref={(input) => this.name = input}
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 
 Tweet.propTypes = {
